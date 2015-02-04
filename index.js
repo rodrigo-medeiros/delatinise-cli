573d27c48f1a2837115350f8b2f4d365a09f5658
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2015-02-03 23:49:38 -0200

    Small refactoring.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ program
 	.version('0.1.0')
 	.description("A CLI tool to remove accents from text files.")
 	.usage('[options] <path>')
-	.option('-d, --delete', "Delete original files.")
 	.option('-e, --encoding [type]', "The desired encoding output [utf8].", 'utf8')
 	.parse(process.argv);
 
@@ -19,6 +18,6 @@ var options = {
 var delatinise = new Delatinise(options);
 
 delatinise.run(function (error) {
-  if (error) console.log(error);
-  else console.log("All files have been successfully processed.");
+  if (error) process.stdout.write(error + "\n");
+  else process.stdout.write("All files have been successfully processed.\n");
 });
--- test/test.js
+++ test/test.js
@@ -1,51 +1,64 @@
 var Delatinise = require('../lib/delatinise'),
-    expect = require('expect.js');
+    expect = require('expect.js'),
+    exec = require('child_process').exec;
 
-describe("validatePath:", function () {
+describe("CLI", function () {
+  beforeEach(function (done) {
+    copyFiles(done);
+  });
 
-  describe("When a valid path is passed", function () {
+  afterEach(function (done) {
+    removeFilesAndDir(done);
+  });
 
-    it("should return true", function (done) {
-      var options = {
-        filePath: "./"
-      };
-      var delatinise = new Delatinise(options);
-      delatinise.validatePath(function (isValid) {
-        expect(isValid).to.be(true);
+  describe("When a valid file is passed", function () {
+
+    it("should process the file and return a confirmation message", function (done) {
+
+      exec("node index.js ./test/with-latin-chars.txt", function (error, stdout, stderr) {
+        if (error) return done(error);
+        expect(stdout).to.equal("All files have been successfully processed.\n");
         done();
       });
+
     });
 
   });
 
-  describe("When an invalid path is passed", function () {
+  describe("When an invalid file is passed", function () {
 
-    it("should return false", function (done) {
-      var options = {
-        filePath: "./foo/bar"
-      };
-      var delatinise = new Delatinise(options);
-      delatinise.validatePath(function (isValid) {
-        expect(isValid).to.be(false);
+    it("should not process the file and return an error message", function (done) {
+
+      exec("node index.js ./foo/bar/invalid-file.txt", function (error, stdout, stderr) {
+        if (error) return done(error);
+        expect(stdout).to.equal("An error has ocurred: Directory/file ./foo/bar/invalid-file.txt doesn't exist.\n");
         done();
       });
+
     });
 
   });
 
   describe("When no path is passed", function () {
 
-    it("should return true", function (done) {
-      var options = {
-        filePath: ""
-      };
-      var delatinise = new Delatinise(options);
-      delatinise.validatePath(function (isValid) {
-        expect(isValid).to.be(true);
-        done();
-      });
+    it("should process all .txt files inside current dir and return a confirmation message", function (done) {
+      done();
     });
 
   });
 
 });
+
+function copyFiles (done) {
+  exec("cp ./test/files/with-latin-chars.txt ./test/;", function (error, stdout, sdterr) {
+    if (error) return done(error);
+    done();
+  });
+}
+
+function removeFilesAndDir (done) {
+  exec("rm -rf ./test/backup; rm -f ./test/with-latin-chars.txt", function (error, stdout, sdterr) {
+    if (error) return done(error);
+    done();
+  });
+}
